d74a42325587ccf4e2c33c07cbd57205fb377db3
Gustibimo <gstbimo@gmail.com>
2019-07-06 12:24:46 +0200

    Update Predict class

--- app.py
+++ app.py
@@ -21,3 +21,36 @@ with open(vec_path, 'rb') as f:
 parser = reqparse.RequestParser()
 parser.add_argument('query')
 
+
+class PredictSentiment(Resource):
+    def get(self):
+        # use parser and find the user's query
+        args = parser.parse_args()
+        user_query = args['query']
+
+        # vectorize the user's query and make a prediction
+        uq_vectorized = model.vectorizer_transform(np.array([user_query]))
+        prediction = model.predict(uq_vectorized)
+        pred_proba = model.predict_proba(uq_vectorized)
+
+        # Output either 'Negative' or 'Positive' along with the score
+        if prediction == 0:
+            pred_text = 'Negative'
+        else:
+            pred_text = 'Positive'
+
+        # round the predict proba value and set to new variable
+        confidence = round(pred_proba[0], 3)
+
+        # create JSON object
+        output = {'prediction': pred_text, 'confidence': confidence}
+
+        return output
+
+
+# Setup the Api resource routing here
+# Route the URL to the resource
+api.add_resource(PredictSentiment, '/')
+
+if __name__ == '__main__':
+    app.run(debug=True)
